d35b46491a1dd25e47c8f78e3a7e2bdcf0ba0b7a
oleeriksen <oe@easv.dk>
2022-02-25 09:37:35 +0100

    minor stuff

--- ConsoleSearch/SearchResult.cs
+++ ConsoleSearch/SearchResult.cs
@@ -4,6 +4,14 @@ using CommonStuff.BE;
 
 namespace ConsoleSearch
 {
+    /*
+     * A data class representing the result of a search.
+     * Hits is the total number of documents containing at least one word from the query.
+     * DocumentHits is the documents and the number of words from the query contained in the document - see
+     * the class DocumentHit
+     * Ignored contains words from the query not present in the document base.
+     * TimeUsed is the timespan used to perform the search.
+     */
     public class SearchResult
     {
         public SearchResult(int hits, List<DocumentHit> documents, List<string> ignored, TimeSpan timeUsed)
